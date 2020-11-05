b57db6367300158d2140de448346f4664d600b4a
vortex7 <sean.wright.vortex@gmail.com>
2020-11-05 15:39:11 -0500

    deleted:    config/web/assets/app/package-lock.json deleted:    config/web/assets/app/package.json modified:   config/web/assets/app/src/components/flow-manager.js modified:   react-web/app/package.json modified:   react-web/app/src/components/flow-manager.js

--- config/web/assets/app/src/components/flow-manager.js
+++ config/web/assets/app/src/components/flow-manager.js
@@ -1,30 +1,19 @@
 import React from "react"
 import { withStyles } from "@material-ui/core/styles"
 
-import { Stage, Sprite } from "@inlet/react-pixi"
-
-import { useState } from "react";
+import { useState } from "react"
 
 const styles = (theme) => ({
   root: {
   },
 })
 
-const colors = ["#ff0055", "#0099ff", "#22cc88", "#ffaa00"];
-
-const spring = {
-  type: "spring",
-  stiffness: 500,
-  damping: 30
-}
-
 const FlowManager = (props) => {
-  const [selected, setSelected] = useState(colors[0])
 
   return (
-    <Stage>
-      <Sprite image="/logo192.png" x={100} y={100} />
-    </Stage>
+    <div>
+      FlowManager
+    </div>
   )
 }
 
