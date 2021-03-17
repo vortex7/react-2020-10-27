e2b4cd5df719229e1afa4f823f03447e2f419a4e
zerodevgroup <admin@zerodevgroup.com>
2021-03-17 11:28:55 -0400

    deleted:    react-web/src/components/canvas-component.js deleted:    react-web/src/components/flow-animation.js deleted:    react-web/src/components/flow-connector.js deleted:    react-web/src/components/flow-manager.js deleted:    react-web/src/components/flow-node.js

--- react-web/src/components/flow-connector.js
+++
@@ -1,8 +0,0 @@
-const drawConnector = (connector, ctx) => {
-  ctx.strokeStyle = connector.color
-  ctx.moveTo(connector.from.x + (connector.from.width / 2), connector.from.y + (connector.from.height / 2))
-  ctx.lineTo(connector.to.x + (connector.to.width / 2), connector.to.y + (connector.to.height / 2))
-  ctx.stroke()
-}
-
-export { drawConnector }
